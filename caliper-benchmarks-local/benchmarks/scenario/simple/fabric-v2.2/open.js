564e8d93b4db0701fb9669755a87ea62f5ece5de
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-08-30 15:51:57 +0700

    Update open.js

--- caliper-benchmarks-local/benchmarks/scenario/simple/fabric-v2.2/open.js
+++ caliper-benchmarks-local/benchmarks/scenario/simple/fabric-v2.2/open.js
@@ -1,43 +1,41 @@
-'use strict';
+"use strict";
 
-module.exports.info = 'opening accounts';
-const { v1: uuidv4 } = require('uuid')
+module.exports.info = "opening accounts";
+const { v1: uuidv4 } = require("uuid");
 
 let account_array = [];
 
 let bc, contx;
-var txnPerBatch = 1
+var txnPerBatch = 1;
 module.exports.init = function (blockchain, context, args) {
-    if (!args.hasOwnProperty('txnPerBatch')) {
-        args.txnPerBatch = 1;
-    }
-    txnPerBatch = args.txnPerBatch;
-    bc = blockchain;
-    contx = context;
+  if (!args.hasOwnProperty("txnPerBatch")) {
+    args.txnPerBatch = 1;
+  }
+  txnPerBatch = args.txnPerBatch;
+  bc = blockchain;
+  contx = context;
 
-    return Promise.resolve();
+  return Promise.resolve();
 };
 
-
 function generateWorkload() {
-    let workload = [];
-    for (let i = 0; i < txnPerBatch; i++) {
-
-        workload.push({
-            chaincodeFunction: 'createCar',
-            chaincodeArguments: [uuidv4(), 'A', 'B', 'C', "d"],
-        });
-    }
-    return workload;
+  let workload = [];
+  for (let i = 0; i < txnPerBatch; i++) {
+    workload.push({
+      chaincodeFunction: "addTemp",
+      chaincodeArguments: [uuidv4(), "A", "B", "C", "d"],
+    });
+  }
+  return workload;
 }
 
 module.exports.run = function () {
-    let args = generateWorkload();
-    return bc.invokeSmartContract(contx, 'fabcar', '1', args);
+  let args = generateWorkload();
+  return bc.invokeSmartContract(contx, "mycc", "1", args);
 };
 
 module.exports.end = function () {
-    return Promise.resolve();
+  return Promise.resolve();
 };
 
 module.exports.account_array = account_array;
