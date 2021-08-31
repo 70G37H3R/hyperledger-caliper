0c761a680dd642563e8bbdf086ecf0ffb1dd5af2
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-09-01 01:29:17 +0700

    Update create.js

--- caliper-benchmarks-local/benchmarks/scenario/simple/fabric-v2.2/create.js
+++ caliper-benchmarks-local/benchmarks/scenario/simple/fabric-v2.2/create.js
@@ -26,7 +26,9 @@ const Time = [
 ];
 
 const randomNo = [ "0","1","2","3","4","5","6","7","8","9"];
-const randomNo = [ "10","11","12","13","14","15","16","17","18","19"];
+/** for next round 
+*const randomNo = [ "10","11","12","13","14","15","16","17","18","19"];
+*/
 
 /**
  * Workload module for the benchmark round.
